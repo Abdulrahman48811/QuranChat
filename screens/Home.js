beac5f672c9eeda35557edbbed9f3a8846ec15a5
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-01-11 17:51:46 -0800

    still working of home page design

--- screens/Home.js
+++ screens/Home.js
@@ -1,33 +1,34 @@
 import React, { useEffect } from "react";
 import {
-    StyleSheet,
-    Text,
-    Button,
-    View,
-    Image,
-    TouchableOpacity,
-  } from 'react-native';
+  StyleSheet,
+  Text,
+  Button,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { signOut } from "firebase/auth";
-// import { FontAwesome } from "expo-vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 import { AntDesign, Entypo } from "@expo/vector-icons";
 import { auth } from "../config/firebase";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const topLogo = require("../assets/logo.png");
+const fatiha = require("../assets/quranimg.jpeg");
 
 const onSignOut = () => {
-    signOut(auth).catch((error) => console.log(error));
-  };
+  signOut(auth).catch((error) => console.log(error));
+};
 
 const Home = () => {
+  const navigation = useNavigation();
 
-    const navigation = useNavigation();
-
-    useEffect(() => {
-      navigation.setOptions({
-        headerRight: () => (
-            // <Text>Hi</Text>
-            <TouchableOpacity
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        // <Text>Hi</Text>
+        <TouchableOpacity
           style={{
             marginRight: 10,
           }}
@@ -40,73 +41,85 @@ const Home = () => {
             style={{ marginRight: 10 }}
           />
         </TouchableOpacity>
-            // <FontAwesome name="search" size={24} color="gray" style={{marginLeft: 15}}/>
-        ),
-        headerStyle: {
-            backgroundColor: '#F5F3F3'
-          },
-        headerLeft: () => (
-            <Image 
-            source={topLogo}
-            style={{
-                width: 40,
-                height: 40,
-                marginLeft: 15,
-            }}/>
-
-            ),
-        });
-    }, [navigation]);
-    return (
-        <View style={styles.chatview}>
-            <View style={styles.container}>
-
-                <Text>HI</Text>
-                
-            </View>
-            <Text style={{top: -65, flex:5, fontSize: 15, color:"#4C4646", }}>Welcome To The Main Chat Room!</Text>
-            <TouchableOpacity
-                onPress={() => navigation.navigate("Chat")}
-                style={styles.chatButton}
-                >
-                <Entypo name="chat" size={24} color="blue"/>
-            </TouchableOpacity>
-        </View>
-        )
-}
+        // <FontAwesome name="search" size={24} color="gray" style={{marginLeft: 15}}/>
+      ),
+      headerStyle: {
+        backgroundColor: "#F5F3F3",
+      },
+      headerLeft: () => (
+        <Image
+          source={topLogo}
+          style={{
+            width: 40,
+            height: 40,
+            marginLeft: 15,
+          }}
+        />
+      ),
+    });
+  }, [navigation]);
+  return (
+    <View style={styles.chatview}>
+      <View style={styles.container}>
+        <Image
+          source={fatiha}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: 280,
+            height: 170,
+          }}
+        />
+      </View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Chat")}
+        style={styles.chatButton}
+      >
+        <Entypo name="chat" size={22} color="orange" style={styles.icon1}>
+          <Text style={styles.text}>
+            {"  "}
+            Chat Room{"                                 "}
+          </Text><Entypo
+              name="chevron-right"
+              size={22}
+              color="white"
+            ></Entypo></Entypo>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default Home;
 
 const styles = StyleSheet.create({
-    chatview: {
-        flex: 1,
-        justifyContent: "flex-end",
-        alignItems: "flex-end",
-        backgroundColor: "white",
-        flexDirection: 'row',
-    },
-    container: {
-        flex: 1,
-        justifyContent: "flex-start",
-        alignItems: "flex-start",
-        backgroundColor: "white",
-    },
-    chatButton: {
-        backgroundColor: "yellow",
-        height: 50,
-        width: 50,
-        borderRadius: 25,
-        alignItems: "center",
-        justifyContent: "center",
-        shadowColor: "orange",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: .9,
-        shadowRadius: 8,
-        marginRight: 20,
-        marginBottom: 50,
-
-    },
-})
+  chatview: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  container: {
+    flex: 1,
+    // justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  chatButton: {
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 15,
+    backgroundColor: "lightgray",
+    marginRight: 35,
+    marginLeft: 35,
+    marginBottom: 50,
+  },
+  text: {
+    fontSize: 15,
+    fontWeight: "900",
+    fontFamily: "Arial",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+  icon1: {
+    marginLeft: -25,
+  },
+});
