18d19ab4bb2845daa859e9640715775a136c26dc
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-01-14 22:40:59 -0800

    fixed navigation container for login&signup

--- App.js
+++ App.js
@@ -1,105 +1,111 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 import { View, ActivityIndicator } from "react-native";
-import { onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged } from "firebase/auth";
 import Chat from "./screens/Chat";
-import Login from './screens/Login';
+import Login from "./screens/Login";
 import Home from "./screens/Home";
-import Settings from './screens/Settings';
-import Signup from './screens/Signup';
+import Settings from "./screens/Settings";
+import Signup from "./screens/Signup";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 import { auth } from "./config/firebase";
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 const Stack = createStackNavigator();
 const AuthenticatedUserContext = createContext({});
 const Tab = createBottomTabNavigator();
 const homeName = "Home";
-const settingsName = 'Settings';
-
+const settingsName = "Settings";
+const chatName = "Chat";
 
 const AuthenticatedUserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   return (
-    <AuthenticatedUserContext.Provider value={{user, setUser}}>
+    <AuthenticatedUserContext.Provider value={{ user, setUser }}>
       {children}
     </AuthenticatedUserContext.Provider>
-  )
-}
+  );
+};
 
 function ChatStack() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-      initialRouteName={homeName}
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({focused, color, size}) => {
-          let iconName;
-          let rn = route.name;
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
 
-          if(rn === homeName) {
-            iconName = focused ? 'home' : 'home-outline'
-          } else if (rn === settingsName) {
-            iconName = focused ? 'settings' : 'settings-outline'
-          }
+            if (rn === homeName) {
+              iconName = focused ? "home" : "home-outline";
+            } else if (rn === chatName) {
+              iconName = focused
+                ? "chatbox-ellipses-outline"
+                : "chatbox-ellipses-outline";
+            } else if (rn === settingsName) {
+              iconName = focused ? "settings" : "settings-outline";
+            }
 
-          return <Ionicons name={iconName} size={size} color={color} />
-        },
-      })}
-
-      tabBarOptions={{
-        activeTintColor: 'gold',
-        inactiveTintColor: 'gray',
-        labelStyle: { paddingBottom: 10, fontSize: 10},
-        style: {padding: 10, height: 70}
-      }}
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        // eslint-disable-next-line
+        tabBarOptions={{
+          activeTintColor: "#da9100",
+          inactiveTintColor: "gray",
+          labelStyle: { paddingBottom: 1, fontSize: 10 },
+          style: { padding: 10, height: 70 },
+        }}
       >
-    {/* <Stack.Navigator defaultScreenOptions={Home} > */}
-      <Tab.Screen name={homeName} component={Home}/>
-      <Tab.Screen name={settingsName} component={Settings} />
-      <Stack.Screen name='Chat' component={Chat}/>
+        {/* <Stack.Navigator defaultScreenOptions={Home} > */}
+        <Tab.Screen name={homeName} component={Home} />
+        <Stack.Screen name="Chat" component={Chat} />
+        <Tab.Screen name={settingsName} component={Settings} />
       </Tab.Navigator>
-    {/* /* </Stack.Navigator> */ }
+      {/* /* </Stack.Navigator> */}
     </NavigationContainer>
-  )
+  );
 }
 
-function AuthStack () {
+function AuthStack() {
   return (
     <NavigationContainer>
-  <Stack.Navigator defaultScreenOptions={Login} screenOptions={{ headerShown: false }}>
-  <Stack.Screen name='Login' component={Login}/>
-  <Stack.Screen name='Signup' component={Signup}/>
-</Stack.Navigator>
-</NavigationContainer>
-  )
+      <Stack.Navigator
+        defaultScreenOptions={Login}
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 function RootNavigator() {
-  const { user, setUser} = useContext(AuthenticatedUserContext);
+  const { user, setUser } = useContext(AuthenticatedUserContext);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth,
-      async authenticatedUser => {
-        authenticatedUser ? setUser(authenticatedUser) : setUser(null);
-        setLoading(false);
-      })
-      return () => unsubscribe();
+    const unsubscribe = onAuthStateChanged(auth, async (authenticatedUser) => {
+      authenticatedUser ? setUser(authenticatedUser) : setUser(null);
+      setLoading(false);
+    });
+    return () => unsubscribe();
   }, [user]);
-  if(loading) {
+  if (loading) {
     return (
-      <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <ActivityIndicator size="large" />
       </View>
-    )
+    );
   }
   return (
     // <NavigationContainer>
-       user ? <ChatStack /> : <AuthStack />
+    user ? <ChatStack /> : <AuthStack />
     // </NavigationContainer>
-  )
+  );
 }
 
 export default function App() {
@@ -107,5 +113,5 @@ export default function App() {
     <AuthenticatedUserProvider>
       <RootNavigator />
     </AuthenticatedUserProvider>
-  ) 
+  );
 }
