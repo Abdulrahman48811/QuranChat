60340c814ba3aa9a02b897224c2c0be20b0b7be6
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-01-29 23:59:13 -0800

    prettier

--- screens/Login.js
+++ screens/Login.js
@@ -30,108 +30,121 @@ export default function Login({ navigation }) {
 
   return (
     <View style={styles.container}>
-        <Image source={backImage} style={styles.backImage} />
-        <View style={styles.whiteSheet} />
-        <SafeAreaView style={styles.form}>
-            <Text style={styles.title}>Login</Text>
-            <TextInput
-            style={styles.input}
-            placeholder="Enter Email"
-            autoCapitalize="none"
-            keyboardType="email-address"
-            textContentType="emailAddress"
-            autoFocus={true}
-            value={email}
-            onChangeText={(text) => setEmail(text)}
-            />
-            <TextInput
-            style={styles.input}
-            placeholder="Enter Password"
-            autoCapitalize="none"
-            autoCorrect={false}
-            secureTextEntry={true}
-            textContentType="password"
-            value={password}
-            onChangeText={(text) => setPassword(text)}
-            />
-            <TouchableOpacity style={styles.button} onPress={onHandleLogin}>
-                <Text style={{fontWeight: "bold", color: "#fff", fontSize: 18}}>Log In</Text>
-            </TouchableOpacity>
-            <View style={{marginTop: 20, flexDirection: "row", alignItems: "center", alignSelf: "center"}}>
-                <Text style={{color: "gray", fontWeight: "600", fontSize: 14}}>Don't have an account?</Text>
-                <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
-                    <Text style={{color: "#da9100", fontWeight: "600", fontSize: 14}}> Sign Up</Text>
-                </TouchableOpacity>
-                </View>
-        </SafeAreaView>
-        <Text
+      <Image source={backImage} style={styles.backImage} />
+      <View style={styles.whiteSheet} />
+      <SafeAreaView style={styles.form}>
+        <Text style={styles.title}>Login</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Email"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+          autoFocus={true}
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Password"
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry={true}
+          textContentType="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <TouchableOpacity style={styles.button} onPress={onHandleLogin}>
+          <Text style={{ fontWeight: "bold", color: "#fff", fontSize: 18 }}>
+            Log In
+          </Text>
+        </TouchableOpacity>
+        <View
           style={{
-            color: "#da9100",
-            fontSize: 18,
-            fontFamily: "Cochin",
-            fontWeight: "bold",
-            marginBottom: 35,
-            padding: 15,
-            textAlign: 'center',
+            marginTop: 20,
+            flexDirection: "row",
+            alignItems: "center",
+            alignSelf: "center",
           }}
         >
-          <Text style={{ color: "#4C4646" }}>Welcome To </Text>Quran Chat
-          <Text style={{ color: "#4C4646" }}>!</Text>
-        </Text>
+          <Text style={{ color: "gray", fontWeight: "600", fontSize: 14 }}>
+            Don't have an account?
+          </Text>
+          <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
+            <Text style={{ color: "#da9100", fontWeight: "600", fontSize: 14 }}>
+              {" "}
+              Sign Up
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+      <Text
+        style={{
+          color: "#da9100",
+          fontSize: 18,
+          fontFamily: "Cochin",
+          fontWeight: "bold",
+          marginBottom: 35,
+          padding: 15,
+          textAlign: "center",
+        }}
+      >
+        <Text style={{ color: "#4C4646" }}>Welcome To </Text>Quran Chat
+        <Text style={{ color: "#4C4646" }}>!</Text>
+      </Text>
     </View>
-  )
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "fff",
-    },
-    title: {
-        fontFamily: "Cochin",
-        fontSize: 36,
-        fontWeight: "bold",
-        color: "#da9100",
-        alignSelf: "center",
-        paddingBottom: 24,
-        paddingTop: 40,
-    },
-    input: {
-        backgroundColor: "#f6f7fb",
-        height: 58,
-        marginBottom: 20,
-        fontSize: 16,
-        borderRadius: 10,
-        padding: 12,
-    },
-    backImage: {
-        width: 200,
-        height: 200,
-        position: "absolute",
-        alignSelf: "center",
-        top: 41,
-        resizeMode: "cover",
-    },
-    whiteSheet: {
-        width: "100%",
-        height: "72%",
-        position: "absolute",
-        bottom: 0,
-        backgroundColor: "#fff",
-        borderTopLeftRadius: 60,
-    },
-    form: {
-        flex: 1,
-        justifyContent: "center",
-        marginHorizontal: 30,
-    },
-    button: {
-        backgroundColor: "#da9100",
-        height: 50,
-        borderRadius: 10,
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 40,
-    },
-})
+  container: {
+    flex: 1,
+    backgroundColor: "fff",
+  },
+  title: {
+    fontFamily: "Cochin",
+    fontSize: 36,
+    fontWeight: "bold",
+    color: "#da9100",
+    alignSelf: "center",
+    paddingBottom: 24,
+    paddingTop: 40,
+  },
+  input: {
+    backgroundColor: "#f6f7fb",
+    height: 58,
+    marginBottom: 20,
+    fontSize: 16,
+    borderRadius: 10,
+    padding: 12,
+  },
+  backImage: {
+    width: 200,
+    height: 200,
+    position: "absolute",
+    alignSelf: "center",
+    top: 41,
+    resizeMode: "cover",
+  },
+  whiteSheet: {
+    width: "100%",
+    height: "72%",
+    position: "absolute",
+    bottom: 0,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 60,
+  },
+  form: {
+    flex: 1,
+    justifyContent: "center",
+    marginHorizontal: 30,
+  },
+  button: {
+    backgroundColor: "#da9100",
+    height: 50,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
+});
