c77cb64c0b9c17c0d89870401668a8b4aa663999
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-01-30 23:05:44 -0800

    added pfp img, change to user

--- screens/Settings.js
+++ screens/Settings.js
@@ -10,7 +10,17 @@
 // }
 
 import React from "react";
-import { View, Text, SafeAreaView, ScrollView, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Touchable,
+  Image,
+} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
 
 const SECTIONS = [
   {
@@ -19,30 +29,30 @@ const SECTIONS = [
     items: [
       { icons: "globe", color: "#fe9400", label: "Language", type: "link" },
       {
-        id: 'darkMode',
-        icon: 'moon',
+        id: "darkMode",
+        icon: "moon",
         color: "#007afe",
         label: "Dark Mode",
         type: "toggle",
       },
       {
         id: "wifi",
-        icon: 'wifi',
+        icon: "wifi",
         color: "#007afe",
         label: "Use Wifi",
         type: "toggle",
       },
       { icon: "navigation", color: "#32c759", label: "Location", type: "link" },
       {
-        id: 'showCollaborators',
-        icon: 'users',
+        id: "showCollaborators",
+        icon: "users",
         color: "#32c759",
         label: "Show collaborators",
         type: "toggle",
       },
       {
-        id: 'accessibilityMode',
-        icon: 'airplay',
+        id: "accessibilityMode",
+        icon: "airplay",
         color: "#fd2d54",
         label: "Accessibility mode",
         type: "toggle",
@@ -50,32 +60,47 @@ const SECTIONS = [
     ],
   },
   {
-    header: 'Help',
+    header: "Help",
     icon: "help-circle",
     items: [
-      { icon: 'flag', color: '#8e8d91', label: 'Report Bug', type: 'link'},
-      { icon: 'mail', color: '#007afe', label: 'Contact Us', type: 'link'},
+      { icon: "flag", color: "#8e8d91", label: "Report Bug", type: "link" },
+      { icon: "mail", color: "#007afe", label: "Contact Us", type: "link" },
     ],
   },
   {
-    header: 'Content',
-    icon: 'align-center',
+    header: "Content",
+    icon: "align-center",
     items: [
-      { icon: 'save', color: '#32c759', label: 'Saved', type: 'link'},
-      { icon: 'download', color: '#fd2d54', label: 'Downloads', type: 'link'},
+      { icon: "save", color: "#32c759", label: "Saved", type: "link" },
+      { icon: "download", color: "#fd2d54", label: "Downloads", type: "link" },
     ],
   },
 ];
 
-const PROFILE_PICTURE = '';
+const PROFILE_PICTURE = "https://cdn.sanity.io/images/vleyd5bv/production/271848966f956012881d89afa883dfc54d87d7bb-1186x1186.jpg";
 
 export default function Settings() {
-  return(
-    <SafeAreaView style={{flex: 1}}>
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
       <ScrollView contentContainerStyle={styles.container}>
         <View style={styles.profile}>
+          <TouchableOpacity
+            onPress={() => {
+              //handle onPress
+            }}
+          >
+            <View style={styles.profileAvatarWrapper}>
+              <Image
+                alt="Profile Picture"
+                source={{ uri: PROFILE_PICTURE }}
+                style={styles.profileAvatar}
+              />
+            </View>
+          </TouchableOpacity>
           <Text style={styles.profileName}>Abdul Mohd</Text>
-          <Text style={styles.profileAddress}>48811 DD Street, RDTown, SD, 99112</Text>
+          <Text style={styles.profileAddress}>
+            48811 DD Street, RDTown, SD, 99112
+          </Text>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -85,5 +110,31 @@ export default function Settings() {
 const styles = StyleSheet.create({
   container: {
     paddingVertical: 24,
+  },
+  profile: {
+    padding: 24,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  profileName: {
+    marginTop: 20,
+    fontSize: 19,
+    fontWeight: "600",
+    color: "#414d63",
+    textAlign: "center",
+  },
+  profileAddress: {
+    marginTop: 5,
+    fontSize: 16,
+    color: "#989898",
+    textAlign: "center",
+  },
+  profileAvatar: {
+    width: 72,
+    height: 72,
+    borderRadius: 9999,
+  },
+  profileAvatarWrapper: {
+    position: 'relative'
   }
-})
+});
