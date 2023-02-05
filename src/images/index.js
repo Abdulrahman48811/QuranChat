74f4fa2e46c8b83ec10c3ae646f038df6cc3e5c0
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-02-04 16:09:32 -0800

    edit page

--- src/images/index.js
+++ src/images/index.js
@@ -1,7 +1,7 @@
 const Images = {
-    selfie: require('./moon.png'),
-    searching: require("./crown.png"),
-    instashot: require("./quricon.png"),
-  };
+  selfie: require("./moon.png"),
+  searching: require("./crown.png"),
+  instashot: require("./quricon.png"),
+};
 
-  export default Images;
+export default Images;
